f9b2a2577d04665f3cfe524b3e7cfe23b9a9a24e
guilhermekulkamp <guilhermekulkamp@gmail.com>
2024-02-25 17:35:30 -0300

    :sparkles: feat: manipulação do DOM com js

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -6,5 +6,41 @@
 */
 
 class CalcController {
-    
+
+    constructor(){
+
+        this._displayCalc = "0";
+        this._currentDate;
+        this.initialize();
+
+    }
+
+    initialize() {
+
+        let displayCalcEl = document.querySelector("#display")
+        let dateEl = document.querySelector("#date");
+        let timeEl = document.querySelector("#time");
+
+        displayCalcEl.innerHTML = "0"
+        dateEl.innerHTML = "00/00/0000"
+        timeEl.innerHTML = "00:00"
+
+    }
+
+    get displayCalc() {
+        return this._displayCalc;
+    }
+
+    set displayCalc(value) {
+        this._displayCalc = value;
+    }
+
+    get dataAtual() {
+        return this._currentDate;
+    }
+
+    set dataAtual(value) {
+        this._currentDate = value;
+    }
+
 }
