010345059a29bae4db4a552d9ddff24e413848f3
guilhermekulkamp <guilhermekulkamp@gmail.com>
2024-02-25 15:36:01 -0300

    :tada: Commit inicial

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,10 @@
+/*  Classe de controle para a calculadora
+*   
+*   Por regra o nome da classe tem sua primeira letra em maiúscula
+*   e é utilizado a formatação CamelCase.
+*   Também é interessante incluir a palavra 'Controller' no final
+*/
+
+class CalcController {
+    
+}
